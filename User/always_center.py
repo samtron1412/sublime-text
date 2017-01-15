ff3c1cd50ecaf124ea90ed42415afe6fb0b769f6
Son Tran <samtron1412@gmail.com>
2017-01-14 19:31:44 -0800

    Change on_selection_modified to on_modified

--- User/always_center.py
+++ User/always_center.py
@@ -1,6 +1,6 @@
 import sublime_plugin
 class AlwaysCenterCommand(sublime_plugin.EventListener):
-    def on_selection_modified(self, view):
+    def on_modified(self, view):
         sel = view.sel()
         region = sel[0] if len(sel) == 1 else None
         if region != None:
